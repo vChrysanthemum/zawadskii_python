9669a876c465c949b7a9be0bb98724d781a8fe11
j <j@ioctl.cc>
2015-01-09 17:59:21 +0800

    use jquery-console

--- blog/application/controller/eval.php
+++ blog/application/controller/eval.php
@@ -8,10 +8,4 @@ if (count($q) < 2) {
 $q[1] = str_replace('<br>', '', $q[1]);
 
 $ret = G::$kv_cache->kv_instance->mget(array($q[0], $q[1]));
-if (is_array($ret)) {
-    foreach($ret as &$v) {
-        $v = str_replace("\n", '<br />', $v);
-        $v = str_replace("\r", '<br />', $v);
-    }
-}
 $helper_api->output($ret);
